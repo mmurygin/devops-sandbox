840e0d924fea491a15d8741f3ef807d461e4b454
Maksim Murygin <mail@muryginm.com>
2019-04-08 22:23:48 +0700

    hw02: add tests with nodejs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Exercises for DevOps course
+
+- [HW2: TravisCI and ChatOps](hw02)
---
+++ hw02/test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const assert = require('assert');
+
+
+describe('should run first test without error', () => {
+    it('some test', () => {
+        assert.equal(4,4);
+    });
+});
