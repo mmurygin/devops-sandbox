44a139c2c60bc2c521fb72bf7db7f2881124c763
Maksim Murygin <mail@muryginm.com>
2019-05-19 07:42:01 +0700

    add tflint to travis ci

--- scripts/install-terraform.sh
+++ scripts/install-terraform.sh
@@ -2,3 +2,6 @@
 
 wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip -O /tmp/terraform.zip
 sudo unzip -o /tmp/terraform.zip -d /usr/local/bin/
+
+wget https://github.com/wata727/tflint/releases/download/v0.7.6/tflint_darwin_amd64.zip -O /tmp/tflint.zip
+sudo unzip -o /tmp/tflint.zip -d /usr/local/bin/
