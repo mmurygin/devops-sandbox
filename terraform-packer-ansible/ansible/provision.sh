b7189a771697a5c4d0a6ca49369b16144631673c
Maksim Murygin <mail@muryginm.com>
2019-05-21 15:33:53 +0700

    setup provisioning with vagrant for tests

--- terraform-packer-ansible/ansible/provision.sh
+++ terraform-packer-ansible/ansible/provision.sh
@@ -10,5 +10,5 @@ if [[ ! -e inventory/gce.ini ]]; then
     echo "Please create inventory/gce.ini and set gcloud configs" >&2 && exit 1
 fi
 
-ansible-playbook -i inventory/gce.py --extra-vars "gce_hosts=tag_reddit-db-${env}" db.yml
-ansible-playbook -i inventory/gce.py --extra-vars "gce_hosts=tag_reddit-app-${env}" app.yml
+ansible-playbook -i inventory/gce.py --extra-vars "dynamic_hosts=tag_reddit-db-${env}" db.yml
+ansible-playbook -i inventory/gce.py --extra-vars "dynamic_hosts=tag_reddit-app-${env}" app.yml
