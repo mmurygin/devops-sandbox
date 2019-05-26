b018ac3b8620ed078eb18f28e7950d33fd562f11
Maksim Murygin <mail@muryginm.com>
2019-05-26 09:58:52 +0700

    add tests for mongodb role

--- terraform-packer-ansible/ansible/roles/mongodb/molecule/default/tests/test_default.py
+++ terraform-packer-ansible/ansible/roles/mongodb/molecule/default/tests/test_default.py
@@ -11,8 +11,12 @@ def test_mongo_running_and_enabled(host):
     assert mongo.is_running
     assert mongo.is_enabled
 
-# # check if configuration file contains the required line
-# def test_config_file(host):
-#     config_file = host.file('/etc/mongod.conf')
-#     assert config_file.contains('bindIp: 0.0.0.0')
-#     assert config_file.is_file
+# check if configuration file contains the required line
+def test_config_file(host):
+    config_file = host.file('/etc/mongod.conf')
+    assert config_file.contains('bindIp: 0.0.0.0')
+    assert config_file.is_file
+
+def test_listening_port(host):
+    socket = host.socket('tcp://0.0.0.0:27017')
+    assert socket.is_listening
