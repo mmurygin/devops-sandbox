f44757219dd52c533db92289c6c3e557595588b9
Maksim Murygin <mail@muryginm.com>
2019-06-29 15:23:03 +0700

    naming refactoring

--- docker-microservices/scripts/build.sh
+++ docker-microservices/scripts/build.sh
@@ -3,6 +3,6 @@
 USER=${1:-muryginm}
 
 docker pull mongo:latest
-docker build -t "$USER"/reddit_post:1.0 ./post-py
-docker build -t "$USER"/reddit_comment:1.0 ./comment
-docker build -t "$USER"/reddit_ui:1.0 ./ui
+docker build -t "$USER"/reddit_post:1.0 ./src/post-py
+docker build -t "$USER"/reddit_comment:1.0 ./src/comment
+docker build -t "$USER"/reddit_ui:1.0 ./src/ui
